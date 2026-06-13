2b239dece225cf457d71154c9a9e1bf620c0df65
MrSpirit-Dev <dev.mrspirit@gmail.com>
2026-06-14 01:22:12 +0530

    bug fixes #2

--- main.py
+++ main.py
@@ -212,6 +212,8 @@ class difficultyMenu(discord.ui.Select):
     name="sudoku-theme",
     description="Change board theme"
 )
+@app_commands.default_permissions(manage_guild=True)
+@app_commands.guilds(*GUILDS)
 @app_commands.choices(
     theme=[
         app_commands.Choice(name="Default", value="default"),
